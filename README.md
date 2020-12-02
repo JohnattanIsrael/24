db53cb873a97a0735d9c583aa4bcd7b58f2e582e
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-12-02 13:58:04 -0600

    Started react redux project template

---
+++ README.md
@@ -0,0 +1,5 @@
+# React/Redux/Router Gamification Project
+
+> Project to push myself into learning complex subjects.
+
+*Johnattan Angeles*
---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Playground</h1>
+        <h2>Yes you can!</h2>
+      </div>
+    );
+  }
+}
