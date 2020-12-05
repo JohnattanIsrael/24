fdebd19447d615c285730a2702926f9048712c6a
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-12-05 17:28:41 -0600

    added footer

--- src/components/app.js
+++ src/components/app.js
@@ -1,27 +1,28 @@
 import React, { Component } from "react";
 
 import DayData from "./dayData";
-import NavBar from './nav';
+import NavBar from "./nav";
 
 export default class App extends Component {
   render() {
     return (
       <div className="home">
         <div className="nav">
-          <NavBar/>
+          <NavBar />
         </div>
 
         <div className="home__content">
           <div className="home__content__left">
-          <DayData />
+            <DayData />
           </div>
           <div className="home__content__right"></div>
         </div>
 
         <div className="day-nav">
-
+          <a href="https://www.linkedin.com/in/johnaangeles/">
+            Johnattan Martínez Angeles - FullStack Web Developer
+          </a>
         </div>
-
       </div>
     );
   }
